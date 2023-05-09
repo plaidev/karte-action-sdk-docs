e885c96e9dcda22919b7c7dd082969f425a0468e
kazuya kawaguchi <kawakazu80@gmail.com>
2023-05-09 14:13:32 +0900

    updates

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vitepress';
 
 export default defineConfig({
   title: 'Karte Action SDK API リファレンス',
-  base: '/karte-action-sdk-docs'
+  base: '/karte-action-sdk-docs/'
 });
